acf10c361d2f7efd544cf6daa14fe4b7a241e037
SquierDotQn <gamefreak35@hotmail.fr>
2013-10-08 19:36:13 +0200

    Update ClientChargen.java
    Virer les ToDo

--- ClientChargen.java
+++ ClientChargen.java
@@ -10,10 +10,8 @@ public class ClientChargen {
 		try {
 			connexion = new Socket(ip, port);
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
